c3bf3850747f27d09e0926dd43cf09d54f61a6c8
GODFATHER-999 <95383244+GODFATHER-999@users.noreply.github.com>
2021-12-02 03:36:59 +0530

    Program to print ASCII values of input characters

---
+++ ASCII value of characters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ using namespace std;
+
+int main ()
+{
+   char c;
+   cout<<"enter a charater:";
+   cin>>c;
+   cout<< "the ASCII value of: "<<c<< " is "<< int(c);
+   return 0;
+
+}
