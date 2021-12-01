fde9c3721c673a13807f32fea0b9435dd6d82add
GODFATHER-999 <95383244+GODFATHER-999@users.noreply.github.com>
2021-12-02 03:37:20 +0530

    Program to multiply two numbers

---
+++ multipyling_two_numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2,prdct;
+
+    cout<<"Enter first number\n";
+    cin>>num1;
+    cout<<"Enter second number\n";
+    cin>>num2;
+    
+    prdct=num1*num2;
+
+    cout<<"The product of "<< num1<<" * "<< num2 << " is = " <<prdct;
+
+    return 0;
+    
+}
